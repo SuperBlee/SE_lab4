6595e7b4a612310d46c9f09b23dac05b42948b46
SuperBlee <purillmit@gmail.com>
2014-10-10 20:32:15 +0800

    This is my first commit!

---
+++ ResultofAuthors.java
@@ -0,0 +1,25 @@
+// This is my first modifying procedure! (In ResultofAuthor)
+//At 2014-10-09
+public class ResultofAuthors {
+	private String name;
+	private int age;
+	private String country;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getCountry() {
+		return country;
+	}
+	public void setCountry(String country) {
+		this.country = country;
+	}
+}
